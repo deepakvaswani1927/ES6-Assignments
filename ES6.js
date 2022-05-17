9776544ab1fd036f69bdd1fda8c491ffa7e72d12
deepakvaswani1927 <62814208+deepakvaswani1927@users.noreply.github.com>
2022-05-18 00:39:33 +0530

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,53 +1,53 @@
 //Program 1-----------------------------------------
-// const age = 24;
-// console.log("Age is :",age);
-// try{
-//     console.log("Increased age is",++age);
-// }
-// catch(e){
-//     console.log("error in try blok was: ",e);
-// }
+const age = 24;
+console.log("Age is :",age);
+try{
+    console.log("Increased age is",++age);
+}
+catch(e){
+    console.log("error in try blok was: ",e);
+}
 
 //Program 2----------------------------------------
-// const j = 20;
-// if(j>0){
-//     let i=10;
-//     i=i+j;
-// }
-// try{
-//     console.log("i is : ",i);
-// }
-// catch(e){
-//     console.log("error: ",e);
-// }
+const j = 20;
+if(j>0){
+    let i=10;
+    i=i+j;
+}
+try{
+    console.log("i is : ",i);
+}
+catch(e){
+    console.log("error: ",e);
+}
 
 //Program 3----------------------------------------
-// function Order(){
-//     this.id = 1,
-//     this.title = 'Icecream',
-//     this.price =100
-// }
-// Order.prototype.printOrder =function(){
-//     return this.id+": " + this.title+": " + this.price
-// }
-// let obj1 = new Order();
-// console.log(obj1.printOrder());
+function Order(){
+    this.id = 1,
+    this.title = 'Icecream',
+    this.price =100
+}
+Order.prototype.printOrder =function(){
+    return this.id+": " + this.title+": " + this.price
+}
+let obj1 = new Order();
+console.log(obj1.printOrder());
 
-// Order.prototype.getPrice =function(){
-//     return this.price;
-// }
-// let obj2 = new Order();
-// console.log("The price is : ",obj2.getPrice());
+Order.prototype.getPrice =function(){
+    return this.price;
+}
+let obj2 = new Order();
+console.log("The price is : ",obj2.getPrice());
 
-// let targetObject = new Order();
-// const source = Order;
+let targetObject = new Order();
+const source = Order;
 
-// const returnedTarget = Object.assign(targetObject, source);
-// console.log("returned target using object.assign  is",returnedTarget)
+const returnedTarget = Object.assign(targetObject, source);
+console.log("returned target using object.assign  is",returnedTarget)
 
 
 //Program 4--------------------------------------------------------
-// let names = ['Tom', 'Ivan', 'Jerry']
-// names.forEach(element => {
-//     console.log("name: ",element,"length: ",element.length);
-// });
+let names = ['Tom', 'Ivan', 'Jerry']
+names.forEach(element => {
+    console.log("name: ",element,"length: ",element.length);
+});
